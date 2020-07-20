ac0bb0140e0d75141500b644cd95acfe69d331a6
lismsinosoft <751204821@qq.com>
2020-07-20 16:22:30 +0800

    优化代码

--- capacity-project/src/main/java/com/sinosoft/capacity/CapacityApplication.java
+++ capacity-project/src/main/java/com/sinosoft/capacity/CapacityApplication.java
@@ -1,12 +1,10 @@
 package com.sinosoft.capacity;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class })
 @MapperScan("com.sinosoft.capacity.db.dao")
